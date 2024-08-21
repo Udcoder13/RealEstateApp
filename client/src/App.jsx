426b64ba9aefb5eb60bcbdd0a131dd4d0afcceeb
Tanishq Kushwaha <tansihqkushwaha2002@gmail.com>
2024-08-21 14:05:10 +0530

    created new repo after deletion

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+
+
+export default function App() {
+  return (
+    <div>App</div>
+  )
+}
---
+++ client/src/main.jsx
@@ -0,0 +1,29 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import App from './App.jsx'
+import './index.css'
+import Home from './Pages/Home.jsx'
+import About from './Pages/About.jsx'
+import Profile from './Pages/Profile.jsx'
+import Login from './Pages/Login.jsx'
+import SignUp from './Pages/Signup.jsx'
+import Layout from './layout.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} >
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="profile" element={<Profile />} />
+      <Route path="login" element={<Login />} />
+      <Route path="signup" element={<SignUp />} />
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
