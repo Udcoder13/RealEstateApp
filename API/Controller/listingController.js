98842a610b836f350bb890880ad357598bb62e1e
Tanishq Kushwaha <tansihqkushwaha2002@gmail.com>
2024-09-02 18:24:46 +0530

    created api route to get user listings

--- API/Controller/listingController.js
+++ API/Controller/listingController.js
@@ -1,4 +1,5 @@
 const listingModel = require('../Models/listing')
+const errorHandler = require('../Utils/errorHandler')
 
 
 const createListing = async(req,res,next)=>{
@@ -10,4 +11,18 @@ const createListing = async(req,res,next)=>{
     }
 }
 
-module.exports = createListing;
+const displayListings = async(req,res,next)=>{
+    if(req.user.id === req.params.id){
+        try {
+            const id = req.params.id;
+            const listings = await  listingModel.find({userId: req.params.id})
+            res.status(200).json(listings)
+        } catch (error) {
+            next(error)
+        }
+    }else{
+        errorHandler(401, "You can get only your listings")
+    }
+}
+
+module.exports = { createListing, displayListings };
--- API/Router/listingRoute.js
+++ API/Router/listingRoute.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const validateUser = require("../Utils/validateUser");
-const createListing = require("../Controller/listingController");
+const { createListing, displayListings }= require("../Controller/listingController");
 const router = express.Router()
 
 router.post("/createListing", validateUser, createListing)
+router.get("/displayListings/:id", validateUser, displayListings)
 
 module.exports = router;
