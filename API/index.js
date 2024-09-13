58cecb855f5d577884d19f00990aec0a50b0947c
Tanishq Kushwaha <tansihqkushwaha2002@gmail.com>
2024-09-13 15:41:54 +0530

    Tanishq

--- API/index.js
+++ API/index.js
@@ -26,11 +26,12 @@ app.use("/api/user", userRoute)
 app.use("/api/update", updateRoute)
 app.use("/api/listing", listingRoute)
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+  res.sendFile(path.join(__dirname, '..', 'client', 'dist', 'index.html'));
+});
+
 
 app.use((error,req,res,next)=>{
     const statusCode = error.statusCode || 500
